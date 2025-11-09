e596eaaaa0022be8823d311f9627d6b1c490ffec
Melody5Sun <sunwenjing513@gmail.com>
2025-11-09 16:33:46 +0000

    #

--- ex1.py
+++ ex1.py
@@ -1,3 +1,21 @@
-def find(s, n):
-# write your implementation here
-    return None
+def find(nums, target):
+    length = len(nums)
+    for i in range(length):
+        for j in range(i + 1, length):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    else:
+        return"No solution"
+
+nums = [2,7,11,15]
+target = 9
+print(find(nums,target))
+nums = [3,2,4]
+target = 6
+print(find(nums,target))
+nums = [3,3]
+target = 6
+print(find(nums,target))
+nums = [3,3]
+target = 7
+print(find(nums,target))
